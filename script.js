c524a1379365f170b35d21c947e929902e9190a2
hardiksainidelhi <hardiksainidelhi@gmail.com>
2025-04-27 07:29:41 +0000

    Impliment randomColor and reduceOpacity functionality

--- script.js
+++ script.js
@@ -1,23 +1,67 @@
 const container = document.querySelector('#container')
-const rowCount = 16
-const colCount = 16
+const GRID_SIDE_LENGTH = 200
+const startButton = document.querySelector('#start')
 
-// create base canvas matrix
-function createGrid(rowCount,colCount){
+startButton.addEventListener('click',drawGrid)
+
+function drawGrid() {
     
-    const gridContainerDiv = document.createElement('div')
-    gridContainerDiv.classList.add('grid')
-    
-    for (let i = 0; i < rowCount; i++){
-        const parentRowDiv = document.createElement('div')
-        for (let j = 0; j < colCount;j++){
-            const childColumnDiv = document.createElement('div')
-            parentRowDiv.appendChild(childColumnDiv)
-        }
-        gridContainerDiv.appendChild(parentRowDiv)
+    container.innerHTML = ''
+    const squarePerSide = +prompt('enter no. of squares per side (atmost 100): ')
+
+    if (squarePerSide > 100){
+        alert('sides must be atmost 100 ')
+        return
     }
 
-    return gridContainerDiv
+    // create base canvas matrix
+    function createGrid(squarePerSide) {
+
+        const gridContainerDiv = document.createElement('div')
+        gridContainerDiv.classList.add('grid')
+
+        for (let i = 0; i < squarePerSide; i++) {
+            const parentRowDiv = document.createElement('div')
+            for (let j = 0; j < squarePerSide; j++) {
+
+                const childColumnDiv = document.createElement('div')
+                childColumnDiv.style.opacity = 1
+
+                parentRowDiv.appendChild(childColumnDiv)
+            }
+            gridContainerDiv.appendChild(parentRowDiv)
+        }
+
+        gridContainerDiv.style.height = `${GRID_SIDE_LENGTH}px`
+        gridContainerDiv.style.width = `${GRID_SIDE_LENGTH}px`
+
+        return gridContainerDiv
+    }
+    
+    // generate random color
+    function createColor() {
+        const randomNumber = () => Math.floor(Math.random()*255)
+        return `rgb(${randomNumber()} ${randomNumber()} ${randomNumber()})`
+    }
+
+    function reduceOpacity(element) {
+        if (element.style.opacity > 0){
+            element.style.opacity -= 0.1
+        }
+    }
+
+    container.appendChild(createGrid(squarePerSide))
+
+    const gridElements = document.querySelectorAll('.grid > div > div')
+
+    gridElements.forEach((element) => {
+        element.addEventListener('mouseenter', () => {
+            element.style.backgroundColor = createColor()
+            reduceOpacity(element)
+            
+        })
+    })
 }
 
-container.appendChild(createGrid(rowCount,colCount))
+
+
